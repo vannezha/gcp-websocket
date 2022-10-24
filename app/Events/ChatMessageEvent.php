348c7852561854512a15ba061b8cf3151c9da61a
vannyezha <vannyezhaa@gmail.com>
2022-10-24 14:19:26 +0700

    init

---
+++ app/Events/ChatMessageEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatMessageEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private string $message;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('public.chat.1');
+    }
+
+    // ini nambahin 2 fungsi buat custome event dan data
+    public function broadcastAs(){
+        return 'chat-message';
+    }
+
+    public function broadcastWith(){
+        return [
+            'message' => $this->message,
+        ];
+    }
+}
---
+++ resources/views/chatapp.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chatapp</title>
+</head>
+<body>
+
+    <ul id="list-messages">
+
+    </ul>
+
+    <form action="" id="chat-form">
+        <label for="input-message">Messages</label>
+        <input type="text" id="input-message">
+    </form>
+
+
+
+    @vite('resources/js/app.js')
+</body>
+</html>
